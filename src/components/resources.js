7ae3722545022733c604c6f2be51438bc8f86ccb
Max <maxnelson997@gmail.com>
2018-03-27 14:15:26 -0600

    init

---
+++ src/components/resources.js
@@ -0,0 +1,16 @@
+//implement shared logic in shared way over multiple components.
+
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            Secret Stuff
+            <ul>
+                <li>its lit</li>
+                <li>its lit</li>
+                <li>its lit</li>
+            </ul>
+        </div>
+    )
+}
